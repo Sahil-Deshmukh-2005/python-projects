95c7fd835babd2f7ab0db88d6f5cc21e306c5234
Sahil Deshmukh <sahildeshmukh9511+cyber@gmail.com>
2026-06-08 01:53:00 +0530

    Add files via upload

--- FileOrganizer/organizer.py
+++ FileOrganizer/organizer.py
@@ -1,3 +1,5 @@
+# ./clear.sh
+
 from pathlib import Path
 import os
 import shutil
@@ -6,20 +8,71 @@ target_directory = Path("/home/sahil/Projects/File Organiser/Test Folder")
 
 file_type_dictionary = {
     "Images" : [".png",".jpg",".jpeg"],
-    "Documents" : [".pdf", ".docx", ".txt"],
+    "Documents" : [".pdf", ".docx", ".doc", ".txt"],
     "Videos" : [".mp4", ".mkv"],
     "Audio" : [".mp3", ".wav"],
     "Archives" : [".zip", ".rar"],
     "Codes" : [".py",".ipynb", ".java", ".c", ".js", ".html", ".css"]
 }
 
-for item in target_directory.iterdir():
-    if item.is_file():
-        file_type = item.suffix.lower()
-        for directory, extensions in file_type_dictionary.items():
-            if file_type in extensions:
-                os.makedirs(f"{target_directory}/{directory}",exist_ok = True)
-                shutil.move(f"{target_directory}/{item.name}", f"{target_directory}/{directory}/{item.name}")
-                print(f"Moved : {item.name} ----> {directory}")
-                break
-        
+moved_count = 0
+error_count = 0
+duplicate_count = 0
+
+def destination(item,directory):
+    error = 0
+    global moved_count
+    global error_count
+    global duplicate_count
+    os.makedirs(target_directory / directory,exist_ok = True)
+    if Path(target_directory / directory / item.name).exists():
+        i = 1
+        while Path(target_directory / directory / f"{item.stem}_{i}{item.suffix}").exists():
+            i += 1
+        try:
+            shutil.move(target_directory / item.name, target_directory / directory / f"{item.stem}_{i}{item.suffix}")
+            duplicate_count += 1
+            moved_count += 1
+        except Exception as e:
+            error = 1
+            print(f"Error: {e}")
+            error_count += 1
+    else:
+        try:
+            shutil.move(target_directory / item.name, target_directory / directory / item.name)
+            moved_count += 1
+        except Exception as e:
+            error = 1
+            print(f"Error: {e}")
+            error_count += 1
+
+    return error
+
+def count():
+    print("="*20 + "Counts" + "="*20)
+    print(f"Total Items Moved: {moved_count}")
+    print(f"Total Error Occurred: {error_count}")
+    print(f"Total Duplicates: {duplicate_count}")
+
+def get_category(file_type,item):
+    flag = 0
+    for directory, extensions in file_type_dictionary.items():
+        if file_type in extensions:
+            flag = 1
+            error = destination(item,directory)
+            break
+    if not flag:
+        directory = "Others"
+        error = destination(item,directory)
+    if not error:
+        print(f"Moved : {item.name} ----> {directory}")
+
+def main():
+    for item in target_directory.iterdir():
+        if item.is_file():
+            file_type = item.suffix.lower()
+            get_category(file_type,item)
+    count()
+    
+
+main()
